629c00ffcdc6d32ad546fe646bff3210384dbab7
Vincent Br <vincent0512@outlook.com>
2022-09-16 19:06:23 +0200

    Minified js

--- dist/static/js/error.js
+++ dist/static/js/error.js
@@ -1,11 +1,1 @@
-"use strict";
-var queryString = window.location.search;
-var urlParams = new URLSearchParams(queryString);
-var errorCode = urlParams.get('id');
-if (errorCode !== undefined && errorCode !== null) {
-    const errorMessage = document.getElementById('errorMessage');
-    if (errorMessage !== null) {
-        errorMessage.innerHTML = `Error ${errorCode}`;
-    }
-}
-//# sourceMappingURL=error.js.map
+"use strict";var queryString=window.location.search,urlParams=new URLSearchParams(queryString),errorCode=urlParams.get("id");if(null!=errorCode){const errorMessage=document.getElementById("errorMessage");null!==errorMessage&&(errorMessage.innerHTML=`Error ${errorCode}`)}
